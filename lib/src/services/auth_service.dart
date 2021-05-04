c17243dfb274edae9535b7e984ef9f0cf4504187
hhacker1999 <harsh.sadh313@gmail.com>
2021-05-04 21:04:16 +0530

    Use 2 factor auth API

--- lib/src/services/auth_service.dart
+++ lib/src/services/auth_service.dart
@@ -3,27 +3,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future signInAutomaticallywithPhoneNumber(String phoneNumber) async {
-    var authResult;
-    try {
-      await _auth.verifyPhoneNumber(
-          phoneNumber: phoneNumber,
-          verificationCompleted: (c) => _signIn,
-          verificationFailed: (e) => throw Exception(e),
-          codeSent: (v, t) => authResult = {'verId': v, 'token': t},
-          codeAutoRetrievalTimeout: (v) => authResult = {'verId': v});
-      return authResult;
-    } catch (e) {
-      throw Exception(e);
-    }
-  }
-
-  Future<void> _signIn(AuthCredential credential) async {
-    try {
-      await _auth.signInWithCredential(credential);
-    } catch (e) {
-      throw e;
-    }
+class AuthService {
+  static const key = "0dd286b6-8882-11eb-a9bc-0200cd936042";
+  Future<bool> requestOtp(String phoneNumber) async {
+    var response = await http.get(
+        Uri.parse('https://2factor.in/API/V1/$key/SMS/$phoneNumber/AUTOGEN'));
+    Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+    String status = decodedResponse['status'];
+    if (status == 'success')
+      return true;
+    else
+      return false;
   }
 
   Future<void> signInwithOtp(String verId, String code) async {
