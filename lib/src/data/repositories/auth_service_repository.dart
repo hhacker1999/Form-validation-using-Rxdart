ab276867182a09a326a1a12bdcf00f707a2d5260
hhacker1999 <harsh.sadh313@gmail.com>
2021-05-04 21:49:00 +0530

    Edit auth service repository to work with new auth service

--- lib/src/data/repositories/auth_service_repository.dart
+++ lib/src/data/repositories/auth_service_repository.dart
@@ -1,40 +1,24 @@
 import 'package:project/src/services/auth_service.dart';
 
 abstract class AuthServiceRepository {
-  Future signInWithPhoneNumber(String phoneNumber);
-  Future<bool> signInWithOtp(String smsCode);
+  Future<bool> signInWithPhoneNumber(String phoneNumber);
+  Future<bool> verifyOtp(String smsCode);
 }
 
 class AuthServiceRepositoryImplementation implements AuthServiceRepository {
   final AuthService _authService;
-  String verId;
 
   AuthServiceRepositoryImplementation(this._authService);
 
   @override
-  Future signInWithPhoneNumber(String phoneNumber) async {
-    var authResult;
-    try {
-      authResult =
-          await _authService.signInAutomaticallywithPhoneNumber(phoneNumber);
-      if (authResult is Map) {
-        authResult['verId'] = verId;
-      }
-    } catch (e) {
-      throw e;
-    }
+  Future<bool> verifyOtp(String smsCode) async {
+    var result = _authService.confirmOtp(smsCode);
+    return result;
   }
 
   @override
-  Future<bool> signInWithOtp(String smsCode) async {
-    var authResult;
-    try {
-      await _authService.signInwithOtp(verId, smsCode);
-      authResult = true;
-    } catch (e) {
-      authResult = false;
-      throw e;
-    }
-    return authResult;
+  Future<bool> signInWithPhoneNumber(String phoneNumber) async {
+    var result = await _authService.requestOtp(phoneNumber);
+    return result;
   }
 }
